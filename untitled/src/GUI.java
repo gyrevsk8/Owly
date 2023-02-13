6b5465724b9de92910dfdf79098b8e8b8b51b5de
gyrevsk8 <gyrevsk8@gmail.com>
2023-02-13 13:46:03 +0300

    Primitive 0.01

---
+++ untitled/src/GUI.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class GUI extends JFrame {
+        GUI()
+        {
+                this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+                this.setBounds(300,300,400,600);
+
+        }
+
+
+        }
---
+++ untitled/src/Master.java
@@ -0,0 +1,13 @@
+public class Master implements Runnable {
+    Master() {
+        GUI gui = new GUI();
+        gui.show();
+    }
+    public void run()
+    {
+
+    }
+
+
+
+}
