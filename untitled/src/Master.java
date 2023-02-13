a655b47c4fa1024de38dfe63376131ca95976e9b
gyrevsk8 <gyrevsk8@gmail.com>
2023-02-13 14:03:27 +0300

    Primitive 0.02

--- untitled/src/Master.java
+++ untitled/src/Master.java
@@ -1,6 +1,6 @@
 public class Master implements Runnable {//стандартный класс-программа
     Master() {
-        WinSet win = null;
+        WinSet win = new WinSet();
         win.setdate("0.0.1980");
         win.setindex(0);
         GUI gui = new GUI(win);
